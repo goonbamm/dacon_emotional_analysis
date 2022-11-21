27888ecbe9034a88e6be00796e8409d54c7387d1
jiyeol park <goonbamm@gmail.com>
2022-11-21 12:34:55 +0900

    [ADD] bash train file

---
+++ train.sh
@@ -0,0 +1,2 @@
+python3 train.py --initial pretrained --pretrained roberta-large --dataset DACON --freeze;
+python3 train.py --initial pretrained --pretrained tae898/emoberta-large --dataset DACON --freeze;
