7860dbc91d86af7cc18c239eb0db8d54c4e4d849
jiyeol park <goonbamm@gmail.com>
2022-11-22 10:49:55 +0900

    [MOVE] CoMPM directory

---
+++ CoMPM/train.sh
@@ -0,0 +1,1 @@
+python3 train.py --initial pretrained --pretrained roberta-large --dataset DACON --batch 64 --freeze --wandb;
--- train.sh
+++
@@ -1,1 +0,0 @@
-python3 train.py --initial pretrained --pretrained roberta-large --dataset DACON --batch 64 --freeze --wandb;
