f13eb3bee6c25d5584ffa2f2af80e2d962c96d5e
jiyeol park <goonbamm@gmail.com>
2022-11-22 11:52:17 +0900

    [ADD] focal loss & modulize

---
+++ CoMPM/loss.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=1, gamma=2, logits=False, reduce=True):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.logits = logits
+        self.reduce = reduce
+
+    def forward(self, inputs, targets):    
+        ce_loss = nn.CrossEntropyLoss()(inputs, targets, reduction='none')
+        pt = torch.exp(-ce_loss)
+
+        F_loss = self.alpha * (1 - pt) ** self.gamma * ce_loss
+
+        if self.reduce:
+            return torch.mean(F_loss)
+
+        else:
+            return F_loss
--- CoMPM/train.py
+++ CoMPM/train.py
@@ -16,6 +16,7 @@ from model import ERC_model
 from torch.cuda.amp import autocast, GradScaler
 from torch.utils.data import Dataset, DataLoader
 from sklearn.metrics import precision_recall_fscore_support
+from loss import FocalLoss
 from utils import make_batch_roberta, make_batch_bert, make_batch_gpt
 from transformers import RobertaTokenizer, get_linear_schedule_with_warmup
 from ERC_dataset import DACON_loader, MELD_loader, Emory_loader, IEMOCAP_loader, DD_loader
@@ -31,13 +32,20 @@ def seed_everything(seed):
     torch.backends.cudnn.benchmark = True
 
 
-def custom_loss(pred_outs, labels):
+def custom_loss(loss_name, pred_outs, labels):
     """
         pred_outs: [batch, clsNum]
         labels: [batch]
     """
-    loss = nn.CrossEntropyLoss()
+
+    if loss_name == 'ce_loss':
+        loss = nn.CrossEntropyLoss()
+    
+    elif loss_name == 'focal_loss':
+        loss = FocalLoss()
+    
     loss_val = loss(pred_outs, labels)
+    
     return loss_val
 
 
@@ -87,6 +95,52 @@ def batch_setting(model_type):
     return make_batch
 
 
+def get_dataloaders(data_path, dataset, DATA_loader, dataclass, batch_size, make_batch, sample):
+    # dataset loading
+    train_path = data_path + dataset+'_train.txt'
+    dev_path = data_path + dataset+'_dev.txt'
+    test_path = data_path + dataset+'_test.txt'
+            
+    train_dataset = DATA_loader(train_path, dataclass)
+
+    if sample < 1.0:
+        train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False, num_workers=4, collate_fn=make_batch)
+
+    else:
+        train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, collate_fn=make_batch)
+
+    train_sample_num = int(len(train_dataloader) * sample)
+    
+    dev_dataset = DATA_loader(dev_path, dataclass)
+    dev_dataloader = DataLoader(dev_dataset, batch_size=1, shuffle=False, num_workers=4, collate_fn=make_batch)
+    
+    test_dataset = DATA_loader(test_path, dataclass)
+    test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=4, collate_fn=make_batch)
+    
+    dataloaders = (train_dataloader, dev_dataloader, test_dataloader)
+    info = (train_dataset.labelList, len(train_dataset), train_sample_num)
+
+    return dataloaders, info
+
+
+def logger_setting(dataset, model_type, initial, freeze, dataclass, sample):
+    save_path = os.path.join(dataset+'_models', model_type, initial,
+                             'freeze' if freeze else 'no_freeze', dataclass, str(sample))
+    
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    
+    print("###Save Path### ", save_path)
+
+    log_path = os.path.join(save_path, 'train.log')
+    fileHandler = logging.FileHandler(log_path)
+    streamHandler = logging.StreamHandler()
+    logger.addHandler(streamHandler)
+    logger.addHandler(fileHandler)    
+    logger.setLevel(level=logging.DEBUG)
+
+    return save_path
+
 def main():
     # device setting
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -105,6 +159,7 @@ def main():
     initial = args.initial
     early_stop = args.earlystop
     use_amp = args.amp
+    loss_name = args.loss
     training_epochs = args.epoch
     max_grad_norm = args.norm
     lr = args.lr
@@ -130,70 +185,39 @@ def main():
         'epochs': training_epochs,
         'max_grad_norm': max_grad_norm,
         'learing_rate': lr,
+        'loss_name': loss_name,
         'seed': seed,
     }
-    
-    # dataset setting
+ 
+    # get dataloaders   
     data_path, DATA_loader = dataset_setting(dataset, args.dyadic)    
-    
-    # batch setting
-    make_batch = batch_setting(model_type)  
-        
-    # freeze setting
-    freeze_type = 'freeze' if freeze else 'no_freeze'
+    make_batch = batch_setting(model_type)
 
-    # dataset loading
-    train_path = data_path + dataset+'_train.txt'
-    dev_path = data_path + dataset+'_dev.txt'
-    test_path = data_path + dataset+'_test.txt'
-            
-    train_dataset = DATA_loader(train_path, dataclass)
+    dataloaders, info = get_dataloaders(data_path, dataset, DATA_loader,
+                                        dataclass, batch_size, make_batch, sample)
 
-    if sample < 1.0:
-        train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False, num_workers=4, collate_fn=make_batch)
-
-    else:
-        train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, collate_fn=make_batch)
-
-    train_sample_num = int(len(train_dataloader) * sample)
-    
-    dev_dataset = DATA_loader(dev_path, dataclass)
-    dev_dataloader = DataLoader(dev_dataset, batch_size=1, shuffle=False, num_workers=4, collate_fn=make_batch)
-    
-    test_dataset = DATA_loader(test_path, dataclass)
-    test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=4, collate_fn=make_batch)
-    
+    train_dataloader, dev_dataloader, test_dataloader = dataloaders    
+    label_list, num_warmup_steps, train_sample_num = info
+    num_training_steps = num_warmup_steps * training_epochs    
+ 
     # logging and path
-    save_path = os.path.join(dataset+'_models', model_type, initial, freeze_type, dataclass, str(sample))
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
+    save_path = logger_setting(dataset, model_type, initial, freeze, dataclass, sample)
     
-    print("###Save Path### ", save_path)
-
-    log_path = os.path.join(save_path, 'train.log')
-    fileHandler = logging.FileHandler(log_path)
-    
-    logger.addHandler(streamHandler)
-    logger.addHandler(fileHandler)    
-    logger.setLevel(level=logging.DEBUG)      
-    
-    """Model Loading"""
-    last = True if 'gpt2' in model_type else False    
-    clsNum = len(train_dataset.labelList)
-    model = ERC_model(model_type, clsNum, last, freeze, initial)
+    # model
+    last = True if 'gpt2' in model_type else False
+    model = ERC_model(model_type, len(label_list), last, freeze, initial)
     model = model.cuda()
     model.train() 
     
-    """Training Setting"""
-    num_training_steps = len(train_dataset) * training_epochs
-    num_warmup_steps = len(train_dataset)
-
+    # optimizer & scheduler
     optimizer = torch.optim.AdamW(model.train_params, lr=lr) # , eps=1e-06, weight_decay=0.01
 
     if not use_amp:
-        scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=num_warmup_steps, num_training_steps=num_training_steps)
+        scheduler = get_linear_schedule_with_warmup(optimizer,
+                                                    num_warmup_steps=num_warmup_steps,
+                                                    num_training_steps=num_training_steps)
     
-    """Input & Label Setting"""
+    # Input & Label Setting
     best_dev_fscore, best_test_fscore = 0, 0
     best_dev_fscore_macro, best_dev_fscore_micro, best_test_fscore_macro, best_test_fscore_micro = 0, 0, 0, 0    
     best_epoch = 0
@@ -221,7 +245,7 @@ def main():
 
             with torch.cuda.amp.autocast(enabled=use_amp):
                 pred_logits = model(batch_input_tokens, batch_speaker_tokens)
-                batch_loss = custom_loss(pred_logits, batch_labels)
+                batch_loss = custom_loss(loss_name, pred_logits, batch_labels)
 
             scaler.scale(batch_loss).backward()
             scaler.unscale_(optimizer)
@@ -240,7 +264,7 @@ def main():
         model.eval()
 
         if dataset == 'dailydialog': # micro & macro
-            dev_acc, dev_pred_list, dev_label_list, dev_loss = _CalACC(model, dev_dataloader)
+            dev_acc, dev_pred_list, dev_label_list, dev_loss = evaluate(loss_name, model, dev_dataloader)
             dev_pre_macro, dev_rec_macro, dev_fbeta_macro, _ = precision_recall_fscore_support(dev_label_list, dev_pred_list, average='macro')
             dev_pre_micro, dev_rec_micro, dev_fbeta_micro, _ = precision_recall_fscore_support(dev_label_list, dev_pred_list, labels=[0,1,2,3,5,6], average='micro') # neutral x
             
@@ -251,12 +275,12 @@ def main():
                 best_dev_fscore_macro = dev_fbeta_macro                
                 best_dev_fscore_micro = dev_fbeta_micro
                 
-                test_acc, test_pred_list, test_label_list, test_loss = _CalACC(model, test_dataloader)
+                test_acc, test_pred_list, test_label_list, test_loss = evaluate(loss_name, model, test_dataloader)
                 test_pre_macro, test_rec_macro, test_fbeta_macro, _ = precision_recall_fscore_support(test_label_list, test_pred_list, average='macro')
                 test_pre_micro, test_rec_micro, test_fbeta_micro, _ = precision_recall_fscore_support(test_label_list, test_pred_list, labels=[0,1,2,3,5,6], average='micro') # neutral x
                 
                 best_epoch = epoch
-                _SaveModel(model, save_path)
+                save_model(model, save_path)
                 
                 patience = 0
                 
@@ -264,7 +288,7 @@ def main():
                 patience += 1
 
         elif dataset == 'DACON': # macro
-            dev_acc, dev_pred_list, dev_label_list, dev_loss = _CalACC(model, dev_dataloader)
+            dev_acc, dev_pred_list, dev_label_list, dev_loss = evaluate(loss_name, model, dev_dataloader)
             dev_pre_macro, dev_rec_macro, dev_fbeta_macro, _ = precision_recall_fscore_support(dev_label_list, dev_pred_list, average='macro')
             
             dev_fscore = dev_fbeta_macro
@@ -273,15 +297,15 @@ def main():
             if dev_fscore > best_dev_fscore_macro:
                 best_dev_fscore_macro = dev_fbeta_macro
 
-                test_acc, test_pred_list, test_label_list, test_loss = _CalACC(model, test_dataloader)
+                test_acc, test_pred_list, test_label_list, test_loss = evaluate(loss_name, model, test_dataloader)
                 test_pre_macro, test_rec_macro, test_fbeta_macro, _ = precision_recall_fscore_support(test_label_list, test_pred_list, average='macro')
 
-                test_pred_list = [train_dataset.labelList[tp] for tp in test_pred_list]
+                test_pred_list = [label_list[tp] for tp in test_pred_list]
                 test_csv = pd.DataFrame(test_pred_list, columns=['Target'])
                 test_csv.to_csv(f'./test_epoch_{epoch}_on_{model_name}.csv', index=False)
 
                 best_epoch = epoch
-                _SaveModel(model, save_path)
+                save_model(model, save_path)
 
                 patience = 0
                 
@@ -289,21 +313,21 @@ def main():
                 patience += 1
 
         else: # weight
-            dev_acc, dev_pred_list, dev_label_list, dev_loss = _CalACC(model, dev_dataloader)
+            dev_acc, dev_pred_list, dev_label_list, dev_loss = evaluate(loss_name, model, dev_dataloader)
             dev_pre, dev_rec, dev_fbeta, _ = precision_recall_fscore_support(dev_label_list, dev_pred_list, average='weighted')
 
             """Best Score & Model Save"""
             if dev_fbeta > best_dev_fscore:
                 best_dev_fscore = dev_fbeta
                 
-                test_acc, test_pred_list, test_label_list, test_loss = _CalACC(model, test_dataloader)
+                test_acc, test_pred_list, test_label_list, test_loss = evaluate(loss_name, model, test_dataloader)
                 test_pre, test_rec, test_fbeta, _ = precision_recall_fscore_support(test_label_list, test_pred_list, average='weighted')
                 
                 test_csv = pd.DataFrame(test_pred_list, columns=['Target'])
                 test_csv.to_csv('./best_test.csv', index=False)
                 
                 best_epoch = epoch
-                _SaveModel(model, save_path)
+                save_model(model, save_path)
 
                 patience = 0
             
@@ -338,7 +362,7 @@ def main():
         logger.info('Final Fscore ## test-accuracy: {}, test-fscore: {}, test_epoch: {}'.format(test_acc, test_fbeta, best_epoch))         
    
     
-def _CalACC(model, dataloader):
+def evaluate(loss_name, model, dataloader):
     model.eval()
     correct = 0
     label_list = []
@@ -353,7 +377,7 @@ def _CalACC(model, dataloader):
             batch_input_tokens, batch_labels = batch_input_tokens.cuda(), batch_labels.cuda()
             
             pred_logits = model(batch_input_tokens, batch_speaker_tokens) # (1, clsNum)
-            batch_loss = custom_loss(pred_logits, batch_labels)
+            batch_loss = custom_loss(loss_name, pred_logits, batch_labels)
             
             """Calculation"""    
             pred_label = pred_logits.argmax(1).item()
@@ -372,7 +396,7 @@ def _CalACC(model, dataloader):
     return acc, pred_list, label_list, np.mean(loss)
 
 
-def _SaveModel(model, path):
+def save_model(model, path):
     if not os.path.exists(path):
         os.makedirs(path)
     torch.save(model.state_dict(), os.path.join(path, 'model.bin'))
@@ -381,7 +405,7 @@ def _SaveModel(model, path):
 if __name__ == '__main__':
     torch.cuda.empty_cache()
     
-    """Parameters"""
+    # parameters
     parser  = argparse.ArgumentParser(description = "Emotion Classifier" )
     parser.add_argument( "--wandb", action='store_true', help='use_wandb')
 
@@ -396,15 +420,14 @@ if __name__ == '__main__':
     parser.add_argument( "--norm", type=int, help = "max_grad_norm", default=10)
     parser.add_argument( "--earlystop", type=int, help = "early stop", default=3)
     parser.add_argument( "--amp", action='store_true', help = "Auto Mixed Precision")
+    parser.add_argument( "--loss", help = 'ce_loss or focal_loss', default = 'ce_loss')
 
     parser.add_argument( "--initial", help = 'pretrained or scratch', default = 'pretrained')
     parser.add_argument( "--pretrained", help = 'roberta-base/roberta-large or bert-large-uncased or gpt2 or gpt2-large or gpt2-medium', default = 'roberta-base')
     parser.add_argument('-fr', '--freeze', action='store_true', help='freezing PM')
         
     args = parser.parse_args()
-    
     logger = logging.getLogger(__name__)
-    streamHandler = logging.StreamHandler()
     
     main()
         
